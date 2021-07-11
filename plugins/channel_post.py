d86cb7c77945a8bd5882d46ef23ad01038c49115
YanspPRIDE <87257753+YanspPRIDE@users.noreply.github.com>
2021-07-11 14:01:07 +0700

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -26,9 +26,9 @@ async def channel_post(client: Client, message: Message):
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
 
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Link Buat Di Share", url=f'https://telegram.me/share/url?url={link}')]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("LINK BUAT DI SHARE", url=f'https://telegram.me/share/url?url={link}')]])
 
-    await reply_text.edit(f"<b>LINK NYA DIKSH KE MEMBER YA TUAN</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
+    await reply_text.edit(f"<b>LINK NYA DIKSH KE MEMBER YA KAK </b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
 
     if not DISABLE_CHANNEL_BUTTON:
         await post_message.edit_reply_markup(reply_markup)
